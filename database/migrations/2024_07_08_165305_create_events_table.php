995400b7e1492c9d464f33d18d94218e1d915b3d
ayamekni <aya.mekni10@gmail.com>
2024-07-09 19:59:16 +0200

    initial migrations

---
+++ database/migrations/2024_07_08_165305_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+        $table->id();
+        $table->string('title'); // Title column
+        $table->text('description')->nullable(); // Description column
+        $table->timestamp('date'); // Date column
+        $table->string('location'); // Location column
+        $table->string('organizer'); // Organizer column
+        $table->timestamps(); // Timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
